c66e19bbe44d8c6bdfb678626a93e73b4260593c
Cecilia <earwen1986@gmail.com>
2021-05-07 08:54:20 +0100

    Started on POST route

--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,49 @@
-/*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+"use strict";
 
-'use strict';
+const { v4: uuidv4 } = require("uuid");
 
 module.exports = function (app) {
-
-  app.route('/api/books')
-    .get(function (req, res){
+  app
+    .route("/api/books")
+    .get(function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+      if (title) {
+        const newBook = {
+          ...req.body,
+          _id: uuidv4(),
+          comments: [],
+        };
+        // TODO write new book to db
+        res.json({ title: newBook.title, _id: newBook._id });
+      } else {
+        res.send("missing required field title");
+      }
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
+  app
+    .route("/api/books/:id")
+    .get(function (req, res) {
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
+
+    .post(function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
 };
